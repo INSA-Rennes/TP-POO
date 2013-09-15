4e15486be544b4e33fadd9af813be6fd114590d3
Paul Chaignon <midionos@laposte.net>
2013-09-15 21:45:45 +0200

    Comments added.

--- TP1/TP1/TP1/Carte.cpp
+++ TP1/TP1/TP1/Carte.cpp
@@ -1,13 +1,11 @@
 /**
-* \ file nom du fichier
-* \ brief breve description de son contenu
-* \ author nom prenom
-* \ version X.Y
-* \ date date de creation
-*
-* Description plus detaillee du contenu du fichier
-*
-*/
+ * \file Carte.cpp
+ * \brief Methodes et initialisation de la classe Carte.
+ * \author Paul Chaignon
+ * \author Xavier Fraboulet
+ * \version 1.0
+ * \date 11/09/13
+ */
 
 #include "Carte.h"
 
@@ -15,10 +13,20 @@ Carte *Carte::teteN = 0;
 Carte *Carte::teteS = 0;
 Carte *Carte::queueN = 0;
 Carte *Carte::queueS = 0;
-	
+
+/**
+ * Tableau avec les correspondances structures - noms pour la methode afficher.
+ */
 std::string hauteurs[] = {"As", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "neuf", "Dix", "Valet", "Dame", "Roi"};
 std::string couleurs[] = {"Pique", "Trefle", "Coeur", "Carreau"};
 
+/**
+ * \fn Carte(Couleur, Hauteur, char)
+ * \brief Constructeur de Carte.
+ * \param[in] c La couleur.
+ * \param[in] h La hauteur.
+ * \param[in] proprio Le proprietaire sous forme de charactere.
+ */
 Carte::Carte(Couleur c, Hauteur h, char proprio): couleur(c), hauteur(h) {
 	if(proprio == 'N') {
 		joueur = Joueur::nord;
@@ -45,30 +53,37 @@ Carte::Carte(Couleur c, Hauteur h, char proprio): couleur(c), hauteur(h) {
 }
 
 /**
-* \fn prototype de la fontion
-* \ brief des
-ription breve de la fon
-tion
-*
-* \ param [in]
-* \ param [out]
-* \ param [in ,out] 
-*
-* \ return le type de retour et sa des
-ription
-*/
+ * \fn bool Carte::supAbs(Carte)
+ * \brief Compare la hauteur d'une carte avec la carte courante.
+ * \param[in] carte La carte a comparer avec la carte courante.
+ * \return Vrai si la carte courante est "superieure" a la carte en parametre.
+ */
 bool Carte::supAbs(Carte carte) {
 	return this->hauteur > carte.hauteur;
 }
 
+/**
+ * \fn bool Carte::egale(Carte)
+ * \brief Compare la hauteur d'une carte avec la carte courante.
+ * \param[in] carte La carte a comparer avec la carte courante.
+ * \return Vrai si les deux carte sont de meme hauteur.
+ */
 bool Carte::egale(Carte carte) {
 	return this->hauteur == carte.hauteur;
 }
 
+/**
+ * \fn void Carte::afficher()
+ * \brief Affiche la carte.
+ */
 void Carte::afficher() {
 	std::cout << hauteurs[this->hauteur-1] << " de " << couleurs[this->couleur - 1] << " (" << this->hauteur << ", " << this->couleur << ")";
 }
 
+/**
+ * \fn void Carte::afficherN()
+ * \brief Affiche l'ensemble des cartes du paquet N.
+ */
 void Carte::afficherN() {
 	Carte *carte = teteN;
 	while(carte != 0) {
@@ -78,6 +93,10 @@ void Carte::afficherN() {
 	}
 }
 
+/**
+ * \fn void Carte::afficherS()
+ * \brief Affiche l'ensemble des cartes du paquet S.
+ */
 void Carte::afficherS() {
 	Carte *carte = teteS;
 	while(carte != 0) {
@@ -87,6 +106,10 @@ void Carte::afficherS() {
 	}
 }
 
+/**
+ * \fn void Carte::passerDerriere()
+ * \brief Passe la carte a la fin du paquet.
+ */
 void Carte::passerDerriere() {
 	if(this->joueur == Joueur::nord) {
 		queueN->succ = this;
@@ -101,6 +124,10 @@ void Carte::passerDerriere() {
 	}
 }
 
+/**
+ * \fn void Carte::changerProp()
+ * \brief Change le proprietaire de la carte.
+ */
 void Carte::changerProp() {
 	if(this->joueur == Joueur::nord) {
 		this->joueur = Joueur::sud;
--- TP1/TP1/TP1/Carte.h
+++ TP1/TP1/TP1/Carte.h
@@ -1,13 +1,11 @@
 /**
-* \ file nom du fichier
-* \ brief breve description de son contenu
-* \ author nom prenom
-* \ version X.Y
-* \ date date de creation
-*
-* Description plus detaillee du contenu du fichier
-*
-*/
+ * \file Carte.h
+ * \brief Delcarations pour la classe Carte.
+ * \author Paul Chaignon
+ * \author Xavier Fraboulet
+ * \version 1.0
+ * \date 11/09/13
+ */
 
 
 #ifndef CARTE_H
@@ -17,6 +15,10 @@
 #include <iostream>
 #include <cassert>
 
+/**
+ * \enum Couleur
+ * \brief Couleur possible pour une carte.
+ */
 enum Couleur {
 	Pique = 1,
 	Trefle,
@@ -24,6 +26,10 @@ enum Couleur {
 	Carreau
 };
 
+/**
+ * \enum Hauteur
+ * \brief Hauteurs possibles pour une carte.
+ */
 enum Hauteur {
 	As = 1,
 	Deux,
@@ -40,23 +46,35 @@ enum Hauteur {
 	Roi
 };
 
+/**
+ * \enum Joueur
+ * \brief Deux joueurs possibles.
+ */
 enum Joueur {
 	nord = 1,
 	sud
 };
 
+/**
+ * \class Carte
+ * \brief Decrit une carte et ses liaisons au paquet.
+ */
 class Carte {
 private:
 	Joueur joueur;
 	Hauteur hauteur;
 	Couleur couleur;
+	/** 
+	 * Successeur de la carte dans le paquet.
+	 */
 	Carte *succ;
+	/** 
+	 * Tetes et queues des deux paquets de cartes.
+	 */
 	static Carte *teteN, *teteS, *queueN, *queueS;
 
 public:
-
 	Carte(Couleur, Hauteur, char);
-
 	Hauteur getHauteur() const;
 	Joueur getJoueur();
 	Couleur getCouleur() const;
@@ -75,34 +93,74 @@ public:
 
 };
 
+/**
+ * \fn inline Hauteur Carte::getHauteur() const
+ * \brief Accesseur pour la hauteur.
+ * \return La hauteur de la carte.
+ */
 inline Hauteur Carte::getHauteur() const {
 	return this->hauteur;
 }
 
+/**
+ * \fn inline Joueur Carte::getJoueur()
+ * \brief Accesseur pour le joueur proprietaire.
+ * \return Le joueur proprietaire de la carte.
+ */
 inline Joueur Carte::getJoueur() {
 	return this->joueur;
 }
 
+/**
+ * \fn inline Couleur Carte::getCouleur() const
+ * \brief Accesseur pour la couleur.
+ * \return La couleur de la carte.
+ */
 inline Couleur Carte::getCouleur() const {
 	return this->couleur;
 }
 
+/**
+ * \fn inline Carte* Carte::getNTete()
+ * \brief Accesseur pour la tete du paquet N.
+ * \return La tete du paquet N.
+ */
 inline Carte* Carte::getNTete() {
 	return Carte::teteN;
 }
 
+/**
+ * \fn prototype de la fontion
+ * \brief Accesseur pour la tete du paquet S.
+ * \return La tete du paquet S.
+ */
 inline Carte* Carte::getSTete() {
 	return Carte::teteS;
 }
 
+/**
+ * \fn prototype de la fontion
+ * \brief Accesseur pour la queue du paquet N.
+ * \return La queue du paquet N.
+ */
 inline Carte* Carte::getNQueue() {
 	return Carte::queueN;
 }
 
+/**
+ * \fn prototype de la fontion
+ * \brief Accesseur pour la queue du paquet S.
+ * \return La queue du paquet S.
+ */
 inline Carte* Carte::getSQueue() {
 	return Carte::queueS;
 }
 
+/**
+ * \fn prototype de la fontion
+ * \brief Accesseur pour le successeur.
+ * \return Le successeur de la carte.
+ */
 inline Carte* Carte::getSucc() {
 	return this->succ;
 }
