e6ae9e8251ee3a2dae9836424ef06328613ce365
Paul Chaignon <paul.chaignon@insa-rennes.fr>
2013-10-02 09:18:24 +0200

    Unit tests added.

--- TP2/TP2/main.cpp
+++ TP2/TP2/main.cpp
@@ -5,7 +5,22 @@ void main() {
 	char *ch = "test";
 	chaine ch1 = chaine(ch);
 	chaine ch2 = chaine(ch1);
+	assert(ch1.size() == ch2.size());
+
 	char c1 = ch1.charAt(1);
 	assert(c1 == 'e');
-	assert
+	char c2 = ch2.charAt(2);
+	assert(c2 == 's');
+
+	chaine ch3 = ch1+ch2;
+	assert(ch3.size() == 2*ch2.size());
+
+	assert(ch3 != ch1);
+	assert(ch3 > ch1);
+	assert(ch3 >= ch1);
+	
+	chaine newch1 = ch3.sous_chaine(0, ch1.size());
+	assert(newch1 == ch1);
+	chaine newch1 = ch3.sous_chaine('t', 't');
+	assert(newch1 == ch1);
 }
