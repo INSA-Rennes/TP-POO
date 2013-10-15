bf587b6951ef3d2b4567f65176b505d8777bd6af
Xavier F <xavier@xavier.res.insa>
2013-10-15 17:48:10 +0200

    Comments remaining for TP3

--- TP3/ADN/ADN/seq_main.cpp
+++ TP3/ADN/ADN/seq_main.cpp
@@ -6,38 +6,56 @@
 */
 
 #include "sequences.h"
-#include "stdafx.h"
 
 using namespace std;
 
 int main()
 {
     string s1,s2,s3;
-
     cout << "Entrez une sequence proteique: ";
     cin >> s1;
     cout << "Entrez son nom: ";
     cin >> s2;
 
-    seqprot P1(s1,s2);
-    cout << P1 << endl;
+	try {
+		seqprot P1(s1,s2);
+		cout << P1 << endl;
+	} catch (std::invalid_argument e) {
+		cout << "Proteine incorrecte, essaye encore : " << e.what() << endl;
+		std::system("pause");
+		return 1;
+	}
 
-    cout << "Entrez une sequence d'adn: ";
+    
+   cout << "Entrez une sequence d'adn: ";
     cin >> s1;
     cout << "Entrez son nom: ";
     cin >> s2;
 
-    seqadn A1(s1,s2);
-    cout << A1 << endl;
+	try {
+		seqadn A1(s1,s2);
+		cout << A1 << endl;
+	} catch (std::invalid_argument e) {
+		cout << "ADN incorrecte, essaye encore : " << e.what() << endl;
+		std::system("pause");
+		return 1;
+	}
 
     cout << "Entrez une sequence d'arn: ";
     cin >> s1;
     cout << "Entrez son nom: ";
     cin >> s2;
 
-    seqarn R1(s1,s2);
-    cout << R1 << endl;
+	try {
+		seqarn R1(s1,s2);
+		cout << R1 << endl;
+	} catch (std::invalid_argument e) {
+		cout << "ARN incorrecte, essaye encore : " << e.what() << endl;
+		std::system("pause");
+		return 1;
+	}
 
+	std::system("pause");
     return 0;
 
 }
